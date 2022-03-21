166a570ce8e9a67b07d4a14a5c8e4ef92641c08b
strtanberahmed <tanber1111@gmail.com>
2022-03-21 14:31:02 +0300

    banner added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import image from '../../../images/banner.jpg';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <div className="">
+            <div className="d-flex">
+                <div className="p-5 row banner">
+                    <div className="col-md-7 text-start text">
+                        <h3>Here at "MSCARE" We Offer</h3>
+                        <h1>World Class Medical Treatment</h1>
+                        <p>The primary goal of our practice is to provide a full range of primary care services to our patients and to do so in a caring and compassionate manner using the latest advances in medical technology and knowledge. As primary care physicians, we are trained to carefully assess a patient’s needs and, working with our patients, determine the most appropriate plan of care or treatment.</p>
+                    </div>
+                    <div className="col-md-5">
+                        <img style={{ width: '25%' }} src={image} alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Appbar from '../../Shared/Appbar/Appbar';
 import Footer from '../../Shared/Footer/Footer';
+import Banner from '../Banner/Banner';
 import Machineries from '../Machineries/Machineries';
 import Services from '../Services/Services';
 
@@ -8,6 +9,7 @@ const Home = () => {
     return (
         <div>
             <Appbar></Appbar>
+            <Banner></Banner>
             <Machineries></Machineries>
             <Services></Services>
             <Footer></Footer>
